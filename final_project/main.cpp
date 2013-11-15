7420e61a277ed009649ba685a762967c97dcd873
Hamza F <hamza93f@gmail.com>
2013-11-15 16:35:56 -0500

    Trying not to overwrite Ashkan B's post

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -14,7 +14,6 @@ using namespace std;
 int main(void){
 	cout << "This is just an empty shell!" << endl;
 	cout << "Trial #2 of upload!" << endl;
-	cout << "Hopefully github is not overwritten" << endl;
 
 	return 0;
 }
