3eb005d2d245c4ccc6035d241955e2f9dab924f9
Sulmaz <sulmaz.zahedi@gmail.com>
2013-11-15 20:15:30 -0500

    Just starting my project

---
+++ Lab4/src/patients.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <iosfwd>
+#include "patients.hpp"
+#include "patients.hpp"
+using namespace std;
+
+
---
+++ Lab4/src/patients.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <iosfwd>
+
+
+#ifndef PATIENTS_HPP_
+#define PATIENTS_HPP_
+
+
+
+
+#endif /* PATIENTS_HPP_ */
