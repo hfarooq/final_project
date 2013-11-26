d277023ba177a9de2c30698374a278a54695c56a
Hamza F <hamza93f@gmail.com>
2013-11-25 22:39:22 -0500

    Main header file

---
+++ final_project/Images.hpp
@@ -0,0 +1,28 @@
+/*
+ * Images.hpp
+ *
+ *  Created on: Nov 25, 2013
+ *      Author: Hamza
+ */
+
+#ifndef IMAGES_HPP_
+#define IMAGES_HPP_
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class Images{
+private:
+	string patient_name;
+	string date;
+
+public:
+	Images();
+
+};
+
+
+
+#endif /* IMAGES_HPP_ */
