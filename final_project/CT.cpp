8041b978b1d659c7732567bdbc3e87771ea112df
Hamza F <hamza93f@gmail.com>
2013-11-25 22:59:33 -0500

    Modified headers with sources

---
+++ final_project/CT.cpp
@@ -0,0 +1,17 @@
+/*
+ * CT.cpp
+ *
+ *  Created on: Nov 25, 2013
+ *      Author: Hamza
+ */
+
+
+#include "CT.hpp"
+
+CT::CT(){
+
+}
+
+CT::CT(string doctor, string person, string schedule){
+
+}
---
+++ final_project/Images.cpp
@@ -0,0 +1,20 @@
+/*
+ * Images.cpp
+ *
+ *  Created on: Nov 25, 2013
+ *      Author: Hamza
+ */
+
+
+#include "Images.hpp"
+
+using namespace std;
+
+Images::Images(){
+
+}
+
+
+Images::Images(string doctor, string person, string schedule){
+
+}
